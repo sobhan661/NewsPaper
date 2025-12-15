fe89b6197daa7c4fdf6104eca88775ed15a83962
sobhan661 <sobhantheimpaler@gmail.com>
2025-12-15 18:19:10 +0330

    ADD : Password change and reset functionality

--- Website/settings.py
+++ Website/settings.py
@@ -144,3 +144,7 @@ LOGOUT_REDIRECT_URL = "home"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Using the backend console as email
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
